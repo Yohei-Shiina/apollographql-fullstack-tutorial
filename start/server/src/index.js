be7ddff38985651b7ed1c67652a025784c997a7f
Yohei Shiina <ysys.j.0125@gmail.com>
2023-04-20 09:23:35 +0000

    update UserAPI

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -29,7 +29,7 @@ const server = new ApolloServer({
   resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
-    userAPI: new UserAPI(store),
+    userAPI: new UserAPI({ store }),
   }),
 });
 
